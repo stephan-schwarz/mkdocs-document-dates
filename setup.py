d0e903aa26d08b1158c0ee12b4b42efa6b056b68
Aaron <aaron@AarondeMacBook-Pro.local>
2025-02-15 14:54:03 +0800

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mkdocs-document-dates",
-    version="0.1.0",
+    version="0.2.0",
     author="Aaron Wang",
     description="一个用于显示文档创建日期和最后修改日期的 MkDocs 插件",
     long_description=long_description,
